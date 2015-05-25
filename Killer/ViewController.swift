f0218fc424b1d5cbff5f263f4db3c8026e7b3b5a
Joe Hicks <joehicks@Joe.local>
2015-05-25 23:49:28 +0100

    Initial Commit

---
+++ Killer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Killer
+//
+//  Created by Joe Hicks on 25/05/2015.
+//  Copyright (c) 2015 Joe Hicks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
